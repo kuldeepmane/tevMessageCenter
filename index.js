5e4d51408fdb966527ba681806f2dd93bbfe3ce1
Kuldeep Name <kuldeepmane94@gmail.com>
2020-09-16 20:06:01 +0530

    function for adding data in mongodb created

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
-exports.helloWorld = (req, res) => {
-  let message = req.query.message || req.body.message || "Hello World!";
-  res.status(200).send(message);
+const mongoose = require("mongoose");
+const MONGODB_URI =
+  "mongodb+srv://owner:password91@cluster0.sptac.mongodb.net/tev?retryWrites=true&w=majority";
+
+let dbConn = null;
+
+function connectToDatabase(uri) {
+  console.log("=> connect to database");
+
+  if (dbConn) {
+    console.log("=> using cached database instance");
+    return Promise.resolve(dbConn);
+  }
+
+  return mongoose
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((db) => {
+      dbConn = db;
+      return dbConn;
+    });
+}
+
+exports.saveMessage = async (req, res) => {
+  try {
+    let db = await connectToDatabase(MONGODB_URI);
+    const message = db.model("messageCenter", {
+      messageTimestamp: { type: Number },
+      message: { type: String },
+      mobileNumber: { type: Number },
+    });
+
+    var newMessage = new message({
+      messageTimestamp: req.body.messageTimestamp,
+      message: req.body.message,
+      mobileNumber: req.body.mobileNumber,
+    });
+
+    let dbResp = await newMessage.save();
+    res.status(200).send(dbResp);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 };
+
+// saveMessage();           //!! use for local testing only
